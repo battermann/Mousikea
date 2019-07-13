1c88132bb15f685521000d069b303f30d83bcea4
Leif Battermann <leif.battermann@gmail.com>
2019-07-13 05:22:04 +0200

    added travis file, correct handling of event start times relative to audio context current time

--- src/index.js
+++ src/index.js
@@ -2,24 +2,25 @@ import { Elm } from './Main.elm'
 import registerServiceWorker from './registerServiceWorker'
 import 'webaudiofont'
 
-var AudioContextFunc = window.AudioContext || window.webkitAudioContext
 
-var app = Elm.Main.init({
+const app = Elm.Main.init({
   node: document.getElementById('root')
 })
 
+const AudioContextFunc = window.AudioContext || window.webkitAudioContext
+const audioContext = new AudioContextFunc()
+const player = new WebAudioFontPlayer()
+
 app.ports.play.subscribe(function (data) {
-  var audioContext = new AudioContextFunc()
-  var player = new WebAudioFontPlayer()
   player.loader.decodeAfterLoading(audioContext, '_tone_0250_SoundBlasterOld_sf2')
 
   data.forEach(function (entry) {
-    console.debug(entry)
-    player.queueWaveTable(audioContext, audioContext.destination, _tone_0250_SoundBlasterOld_sf2, entry.eTime, entry.ePitch, entry.eDur)
+    const vol = entry.eVol / 127
+    const startTime = audioContext.currentTime + 0.1 + entry.eTime
+    player.queueWaveTable(audioContext, audioContext.destination, _tone_0250_SoundBlasterOld_sf2, startTime, entry.ePitch, entry.eDur, vol)
   })
-  player = null
-  audioContext = null
-  return true
+  
+  return false
 })
 
 registerServiceWorker()
