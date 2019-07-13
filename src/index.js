fced2877c6858d97c37d8e56c8aba9f118ab6aa4
Leif Battermann <leif.battermann@gmail.com>
2019-07-13 22:08:40 +0200

    web audio timing

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { Elm } from './Main.elm'
 import registerServiceWorker from './registerServiceWorker'
 import 'webaudiofont'
 
-
 const app = Elm.Main.init({
   node: document.getElementById('root')
 })
@@ -15,12 +14,14 @@ player.loader.decodeAfterLoading(audioContext, '_tone_0250_SoundBlasterOld_sf2')
 app.ports.play.subscribe(function (data) {
   player.cancelQueue(audioContext)
 
+  const startTime = audioContext.currentTime + 0.2
+
   data.forEach(function (entry) {
     const vol = entry.eVol / 127
-    const startTime = audioContext.currentTime + 0.1 + entry.eTime
-    player.queueWaveTable(audioContext, audioContext.destination, _tone_0250_SoundBlasterOld_sf2, startTime, entry.ePitch, entry.eDur, vol)
+    const eTime = startTime + entry.eTime
+    player.queueWaveTable(audioContext, audioContext.destination, _tone_0250_SoundBlasterOld_sf2, eTime, entry.ePitch, entry.eDur, vol)
   })
-  
+
   return false
 })
 
